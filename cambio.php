f9d7d3b20c9ed8817412f21ebb6d5c7f7a392078
Gabriel <gabrielsiano2006@gmail.com>
2024-11-01 15:11:06 +0100

    fix

--- cambio.php
+++ cambio.php
@@ -5,69 +5,68 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
+<body style="text-align: center; background-color: lightblue; font-size: 30px;">
+
 
 <?php
+
+$cambio = array(
+    "USD" => 1.08,
+    "JPY" => 165.93,
+    "CHF" => 0.94,
+    "GBP" => 0.83
+);
 
 $euro = $_GET['EUR'];
 $valuta = $_GET['valuta'];
 $risultato = 0;
 
+echo "<div style='font-family: Arial, sans-serif;'>";
+echo "<p style='font-weight: bold;'>CONVERSIONE EFFETTUATA IL: " . date('d-m-Y ') . "</p>";
 
-date_default_timezone_set('Europe/Rome');
-
-echo "CONVERSIONE EFFETTUATA IL: ";
-echo date('d-m-Y '); 
-
-echo calcolaGiorno(date('l'));
+$giorno = calcolaGiorno(date('l'));
+echo "<p>Giorno: " . $giorno . "</p>";
 echo "<br>";
 
+$commissione = 0;
 
-switch($valuta) {
-    case "USD":
-        $risultato = $euro * 1.08;
-        break;
-    case "JPY":
-        $risultato = $euro * 165.93;
-        break;
-    case "CHF":
-        $risultato = $euro * 0.94;
-        break;
-    case "GBP":
-        $risultato = $euro * 0.83;
-        break;
-
+if (date('w') >= 1 && date('w') <= 5) {  //w = numero del giorno
+    $commissione = $euro * 0.025;
+} else {
+    $commissione = $euro * 0.05;
 }
 
+$euroNettoCommissione = $euro - $commissione;
 
-echo "€ " . $euro . " = " . $risultato . " " . $valuta;
+$risultato = $euroNettoCommissione * $cambio[$valuta];
 
+echo "<p>" . number_format($euro, 2) . " EUR = " . number_format($risultato, 2) . " " . $valuta . "</p>";
+echo "<br>";
+echo "<p>Commissione di cambio: " . number_format($commissione, 2) . " € (" . ($commissione > $euro * 0.025 ? "COMMISSIONE MAGGIORATA" : "commissione standard") . ")</p>";
+echo "<br>";
+echo "<a href='valuta.html' style='color: green; text-decoration: none;'>Torna alla pagina valuta.html</a>";
 
-?>
+echo "</div>";
 
+function calcolaGiorno($day) {
 
-<?php
-
-
-    function calcolaGiorno($day) {
-
-        switch($day) {
-            case "Monday":
-                return "Lunedi";
-            case "Tuesday":
-                return "Martedi";
-            case "Wednesday":
-                return "Mercoledi";
-            case "Thursday":
-                return "Giovedi";
-            case "Friday":
-                return "Venerdi";
-            case "Saturday":
-                return "Sabato";
-            case "Sunday":
-                return "Domenica";
-        }
+    switch ($day) {
+        case "Monday":
+            return "Lunedi";
+        case "Tuesday":
+            return "Martedi";
+        case "Wednesday":
+            return "Mercoledi";
+        case "Thursday":
+            return "Giovedi";
+        case "Friday":
+            return "Venerdi";
+        case "Saturday":
+            return "Sabato";
+        case "Sunday":
+            return "Domenica";
     }
+}
 
 ?>
     
